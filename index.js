55317aaddc20372bbec3aa0570be46a9d8e8e47e
Eduardo Batista <dudubmatos@gmail.com>
2025-05-21 11:49:32 -0300

    vercelJson

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT;
 const app = express();
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/soma", (req, res) => {
   const { num1, num2 } = req.query;
   const result = parseFloat(num1) + parseFloat(num2);
   res.json({ result });
